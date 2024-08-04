3b3cd5dc2eb6cd15d0a4ca102fec8645efbef0c5
Aditya Pratama Badra <orochimadit@gmail.com>
2024-08-05 01:18:42 +0700

    header

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-1">
                                 <Link href="/" className="d-flex align-items-center col-md-12 mb-2 mb-md-0 text-white text-decoration-none">
                                     <img src="/assets/images/logo.png" width="50" />
-                                    <h5 className="mb-0"><strong>Baju </strong> STORE</h5>
+                                    <h5 className="mb-0"><strong>Riena </strong> STORE</h5>
                                 </Link>
                             </header>
                         </div>
